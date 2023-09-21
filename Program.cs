befbd7efcedfc0851cfde49f0e931e3f9390fdcb
lwcsharp <lwcsharp@gmail.com>
2023-09-21 15:51:19 +0200

    wrote down classroom example

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using enums_records;
+
+Day today = Day.Thursday;
+Console.WriteLine(today);
+
+Student student1 = new Student("Benny", "1988-07-09");
+Student student2 = new Student("Dan", "1988-01-15");
+
+Console.WriteLine(student1.Name);
+Console.WriteLine(student2.Birthday);
+Console.WriteLine();
+
+List<Student> students = new List<Student>()
+{
+    new Student("Benny", "1988-07-09"),
+    new Student("Dan", "1988-01-15"),
+    new Student("Arvid", "2003-10-16"),
+    new Student("Manuel", "2000-10-02"),
+};
+
+foreach(Student student in students)
+{
+    Console.WriteLine(student.Name);
+}
---
+++ Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enums_records
+{
+    public record Student(string Name, string Birthday);
+    
+}
